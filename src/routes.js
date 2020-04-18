7a715c2390e741fdaecdb40e1f4a37aea830da1e
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-18 14:56:03 -0300

    Criando rota de post de usuário apenas para testar a conexão

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-
+import User from './app/models/User';
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'App is running'}));
+
+routes.post('/users', async (req, res) => {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+});
 
 
 export default routes;
