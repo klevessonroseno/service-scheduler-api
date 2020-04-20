b7982559e158d60aa52cdfc81e2a8f1588e46cfa
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-20 20:24:02 -0300

    Usando try-catch para tratamento de erro de conexão com a base de dados

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -4,26 +4,31 @@ import authConfig from '../../config/auth';
 
 class SessionController {
     async store(req, res) {
-        const { email, password } = req.body;
+        try {
+            const { email, password } = req.body;
 
-        const user = await User.findOne({ where: { email }});
-
-        if(!(await user.checkPassword(password))) return res.status(401).json({
-            error: 'Password does not math'
-        });
-
-        const { id, name } = user;
-
-        return res.json({
-            user: {
-                id,
-                name,
-                email,
-            },
-            token: jwt.sign({ id }, authConfig.secret, {
-                expiresIn: authConfig.expireIn,
-            }),
-        });
+            const user = await User.findOne({ where: { email }});
+    
+            if(!(await user.checkPassword(password))) return res.status(401).json({
+                error: 'Password does not math'
+            });
+    
+            const { id, name } = user;
+    
+            return res.json({
+                user: {
+                    id,
+                    name,
+                    email,
+                },
+                token: jwt.sign({ id }, authConfig.secret, {
+                    expiresIn: authConfig.expireIn,
+                }),
+            });
+            
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 }
 export default new SessionController();
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,7 +10,7 @@ class UserController {
             if(userExists) return res.status(400).json({
                 error: 'Email must be unique'
             }); 
-               
+
             const { id, name, email, provider } = await User.create(req.body);
     
             res.status(201).json({ id, name, email, provider });
@@ -21,27 +21,32 @@ class UserController {
     }
 
     async update(req, res) {
-        const { email, oldPassword } = req.body;
+        try {
+            const { email, oldPassword } = req.body;
 
-        const user = await User.findByPk(req.userId);
-
-        if(email !== user.email){
-            const userExists = await User.findOne({
-                where: { email }
-            });
-
-            if(userExists) return res.status(400).json({
-                error: 'User email must be unique'
-            });
+            const user = await User.findByPk(req.userId);
+    
+            if(email !== user.email){
+                const userExists = await User.findOne({
+                    where: { email }
+                });
+    
+                if(userExists) return res.status(400).json({
+                    error: 'User email must be unique'
+                });
+            }
+    
+            if(oldPassword && !(await user.checkPassword(oldPassword))){
+                return res.status(401).json({ error: 'Password does not match'});
+            }
+    
+            const { id, name, provider } = await user.update(req.body);
+    
+            return res.json({ id, name, email, provider });
+            
+        } catch (error) {
+            res.status(500).json(error);
         }
-
-        if(oldPassword && !(await user.checkPassword(oldPassword))){
-            return res.status(401).json({ error: 'Password does not match'});
-        }
-
-        const { id, name, provider } = await user.update(req.body);
-
-        return res.json({ id, name, email, provider });
     }
 }
 
