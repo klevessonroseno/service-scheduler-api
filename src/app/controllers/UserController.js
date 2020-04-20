13fdda1fc28aeaab68ad9b05cfbfbcf32ecca4b9
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-20 18:10:28 -0300

    Método store em UserController e chamada desse método em routes

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,22 @@
+import User from '../models/User';
+
+class UserController {
+    async store(req, res) {
+        try {
+            const userExists = await User.findOne({
+                where: { email: req.body.email }
+            });    
+            if(userExists) return res.status(400).json({
+                error: 'Email must be unique'
+            });    
+            const { id, name, email, provider } = await User.create(req.body);
+    
+            res.status(201).json({ id, name, email, provider });
+
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'App is running'}));
 
-routes.post('/users', async (req, res) => {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 
 export default routes;
