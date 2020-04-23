9a8def1123cd0a3fcb785a81595534710fd5ec39
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-23 18:00:33 -0300

    Configurando o controller de File

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,6 +1,17 @@
+import File from '../models/File';
+
 class FileController {
     async store(req, res){
-        return res.json(req.file)
+      try {
+        const { originalname: name, filename: path } = req.file;
+
+        const file = await File.create({ name, path });
+
+        return res.json(file);
+
+      } catch (error) {
+        return res.status(500).json(error);  
+      }
     }
 }
 
