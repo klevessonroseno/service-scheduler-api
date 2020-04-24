1fa5dbcd538fed89f780b39b3175e84c43ccce95
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-24 09:46:24 -0300

    Migration para appointments

---
+++ src/database/migrations/20200424122649-create-appointments.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('appointments', {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        date: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        canceled_at: {
+          type: Sequelize.DATE,
+        },
+        provider_id: {
+            type: Sequelize.INTEGER,
+            references: { model: 'users', key: 'id' },
+            onUpdate: 'CASCADE',
+            unDelete: 'SET NULL',
+            allowNull: true,
+        },
+        user_id: {
+          type: Sequelize.INTEGER,
+          references: { model: 'users', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+          allowNull: true,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      });
+    
+  },
+
+  down: queryInterface => {
+      return queryInterface.dropTable('appointments');
+    
+  }
+};
