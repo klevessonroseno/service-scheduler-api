fe14aee118e7b4bc92fce2631710ba399257407a
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-25 22:37:47 -0300

    Verificando se a data agendada já passou

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -31,6 +31,16 @@ class AppointmentController {
                 });
             }
 
+            /**
+             * Check for past dates
+             */
+
+             const hourStart = startOfHour(parseISO(date));
+
+             if(isBefore(hourStart, new Date())){
+                return res.status(400).json({ error: 'Past dates are not permitted' });
+             }
+
             const appointment = await Appointment.create({
                 user_id: req.userId,
                 provider_id,
