7fc06ff2b905e38fea3646f129697807e2d729f6
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-26 11:08:23 -0300

    Fazendo tratativa de erro na listagem de provedores de serviço

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -3,19 +3,24 @@ import File from '../models/File';
 
 class ProviderController {
     async index(req, res){
-        const providers = await User.findAll({
-            where: { provider: true },
-            attributes: ['id', 'name', 'email', 'avatar_id'],
-            include: [
-                {
-                    model: File,
-                    as: 'avatar',
-                    attributes: ['name', 'path', 'url'],
-                },
-            ],
-        });
+        try {
+            const providers = await User.findAll({
+                where: { provider: true },
+                attributes: ['id', 'name', 'email', 'avatar_id'],
+                include: [
+                    {
+                        model: File,
+                        as: 'avatar',
+                        attributes: ['name', 'path', 'url'],
+                    },
+                ],
+            });
+    
+            return res.status(200).json(providers);
 
-        return res.status(200).json(providers);
+        } catch (error) {
+            return res.status(500).json({ error });
+        }
     }
 }
 
