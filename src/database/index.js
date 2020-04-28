00470abe99070996ce30326100f0ba4f0d0e5ce4
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-28 11:41:59 -0300

    Criando conexão com o mongoDB no projeto

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -9,6 +10,7 @@ const models = [User, File, Appointment];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -17,7 +19,14 @@ class Database {
         models
             .map(model => model.init(this.connection))
             .map(model => model.associate && model.associate(this.connection.models));
-        }
+    }
+
+    mongo(){
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/scheduler',
+            { useNewUrlParser: true, useFindAndModify: true },
+        );
+    }
 }
 
 export default new Database();
