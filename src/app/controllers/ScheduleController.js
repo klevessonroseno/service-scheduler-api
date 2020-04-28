b763af1d9ca4c859616511fa4c503f2e27623cdf
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-28 11:30:02 -0300

    Listando agendamentos do provedor de serviços

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,6 +1,38 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
 class ScheduleController {
     async index(req, res){
-        return;
+        try {
+            const checkUserProvider = await User.findOne({
+                where: { id: req.userId, provider: true },
+            });
+    
+            if(!checkUserProvider) return res.status(400).json({
+                error: 'User is not a provider',
+            });
+            
+            const { date } = req.query;
+            const parsedDate = parseISO(date);
+
+            const appointments = await Appointment.findAll({
+                where: {
+                    provider_id: req.userId,
+                    canceled_at: null,
+                    date: {
+                        [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+                    },
+                },
+                order: ['date'],
+            });
+    
+            return res.status(200).json(appointments);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
     }
 }
 
